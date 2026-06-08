a7f58e1ea7c7049049e83b92533279f556cbb39a
AhmedKSN <redawew9@gmail.com>
2026-06-08 09:17:28 +0300

    sm88xx : fix

--- twrp_sm88XX.mk
+++ twrp_sm88XX.mk
@@ -9,6 +9,8 @@ DEVICE_PATH := device/zte/sm88XX
 # Inherit from device.mk configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
 ## Device identifier
 PRODUCT_DEVICE  := sm88XX
 PRODUCT_NAME    := twrp_sm88XX
